06d6af4a3f31a40df430095b442b6c4905a73655
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-14 16:10:55 -0300

    test(heading): Add all tests for color, lineLeft and lineBottom props

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,14 +1,37 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render a heading in the component', () => {
-    const { container } = render(<Heading />)
-    expect(
-      screen.getByRole('heading', { name: /Heading/i })
-    ).toBeInTheDocument()
+  it('should render a white heading by default', () => {
+    const { container } = renderWithTheme(<Heading>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a black heading when color is passed', () => {
+    const { container } = renderWithTheme(<Heading color={'black'}>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should render lineLeft border when prop is passed', () => {
+    const { container } = renderWithTheme(<Heading color={'black'} lineLeft>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #3CD3C1'
+    })
+  })
+
+  it('should render lineBottom border when prop is passed', () => {
+    const { container } = renderWithTheme(<Heading color={'black'} lineBottom>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after'
+      })
   })
 })
