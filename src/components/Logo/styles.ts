529d6326c6ff875614f7ae2cdc028a76f8aa1342
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-14 11:33:38 -0300

    style(logo): Add mixin to deal with size prop for styled component

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,7 +1,21 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { LogoProps } from '.'
 
+const modifiersMixin = {
+    normal: () => css`
+        width: 11rem;
+        height: 3.3rem;
+    `,
+
+    large: () => css`
+        width: 20rem;
+        height: 5.9rem;
+    `
+}
+
 export const Wrapper = styled.div<LogoProps>`
-    color: ${props => props.theme.colors[props.color!]}
+    color: ${props => props.theme.colors[props.color!]};
+    
+    ${props => !!props.size && modifiersMixin[props.size]}
 `
