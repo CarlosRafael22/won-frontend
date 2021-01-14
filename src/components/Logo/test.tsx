56b8e3519d6d730e291935a19f44f42263c8aae0
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-14 11:34:06 -0300

    test(logo): Add tests for size prop

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -14,11 +14,29 @@ describe('<Logo />', () => {
   })
 
   it('should render black when color is passed', () => {
-    const { container } = renderWithTheme(<Logo color={'black'} />)
+    const { container } = renderWithTheme(<Logo color="black" />)
     expect(
       screen.getByLabelText(/Won Games/i).parentElement
     ).toHaveStyle({
       color: '#030517'
+    })
+  })
+
+  it('should render a normal size as default', () => {
+    const { container } = renderWithTheme(<Logo />)
+    expect(
+      screen.getByLabelText(/Won Games/i).parentElement
+    ).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo when large is passed to size prop', () => {
+    const { container } = renderWithTheme(<Logo size="large" />)
+    expect(
+      screen.getByLabelText(/Won Games/i).parentElement
+    ).toHaveStyle({
+      width: '20rem'
     })
   })
 })
