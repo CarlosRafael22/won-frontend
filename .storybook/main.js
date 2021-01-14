1018a4cff75dc86cff09f09bfca1bd4771242c84
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-14 07:52:03 -0300

    feat(storybook): Add webpackFinal config to have relative import to src/

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
     "../src/components/**/stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-links",
+  addons: [
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
