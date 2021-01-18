dbf9a98f4cf9609caf366be77e0fad17f374d2df
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-18 09:22:01 -0300

    test(button): Create tests for size prop changes

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,14 +1,33 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Button from '.'
 
 describe('<Button />', () => {
-  it('should render a heading in the component', () => {
-    const { container } = render(<Button />)
-    expect(
-      screen.getByRole('heading', { name: /Button/i })
-    ).toBeInTheDocument()
+  it('should render the medium size as default', () => {
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      padding: '0.8rem 3.2rem',
+      height: '4rem',
+      fontSize: '1.4rem'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the small button when size prop was passed', () => {
+    renderWithTheme(<Button size='small'>Buy now</Button>)
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      padding: '0.8rem',
+      height: '3rem',
+      fontSize: '1.2rem'
+    })
+  })
+
+  it('should render the large button when size prop was passed', () => {
+    renderWithTheme(<Button size='large'>Buy now</Button>)
+    expect(screen.getByRole('button')).toHaveStyle({
+      padding: '0.8rem 4.8rem',
+      height: '5rem',
+      fontSize: '1.6rem'
+    })
   })
 })
