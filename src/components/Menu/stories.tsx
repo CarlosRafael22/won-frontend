2eeadc7bd68319c37bdcbf78e1b85aee2e326796
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-03-15 18:28:37 -0300

    feat(Menu): Change stories parameters

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -7,3 +7,10 @@ export default {
 } as Meta
 
 export const Default: Story = (args) => <Menu {...args} />
+
+Default.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
