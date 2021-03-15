22a77dcfcea08ec7a98b827f2ec4ceffc5d6f0ad
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-03-15 18:29:24 -0300

    feat(Menu): Add mobile menu with links and buttons

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,6 +5,7 @@ import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import * as Style from './styles'
 import Logo from 'components/Logo'
+import Button from 'components/Button'
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -28,6 +29,15 @@ const Menu = () => {
 
     <Style.FullMenu aria-hidden={!isOpen} isOpen={isOpen}>
       <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <Style.NavMenu>
+          <Style.NavMenuLink href="#">Item</Style.NavMenuLink>
+          <Style.NavMenuLink href="#">Item 2</Style.NavMenuLink>
+        </Style.NavMenu>
+        <Style.RegisterBox>
+          <Button fullWidth size="large">Log in now</Button>
+          <span>or</span>
+          <Style.CreateAccountLink href="#" title="Sign Up">Sign Up</Style.CreateAccountLink>
+        </Style.RegisterBox>
     </Style.FullMenu>
   </Style.Wrapper>
 )}
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -2,46 +2,153 @@ import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
 export const Wrapper = styled.menu`
-    ${({ theme }) => css`
-        display: flex;
-        align-items: center;
-        padding: ${theme.spacings.small} 0;
-        position: relative;
-    `}
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    padding: ${theme.spacings.small} 0;
+    position: relative;
+    z-index: ${theme.layers.menu};
+  `}
 `
 
 export const LogoWrapper = styled.div`
-    ${media.lessThan('medium')`
-        position: absolute;
-        left: 50%;
-        transform: translateX(-50%);
-    `}
+  ${media.lessThan('medium')`
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  `}
 `
 
 export const IconWrapper = styled.div`
-    ${({ theme }) => css`
-        color: ${theme.colors.white};
-        width: 2.4rem;
-        height: 2.4rem;
-    `}
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    cursor: pointer;
+    width: 2.4rem;
+    height: 2.4rem;
+  `}
 `
 
 export const MenuGroup = styled.div`
-    ${({ theme }) => css`
-        display: flex;
-        flex-grow: 1;
-        justify-content: flex-end;
+  ${({ theme }) => css`
+    display: flex;
+    flex-grow: 1;
+    justify-content: flex-end;
+    align-items: center;
+    > div {
+      margin-left: ${theme.spacings.xsmall};
+    }
+  `}
+`
 
-        > div {
-            margin-left: ${theme.spacings.xxsmall};
+export const NavMenu = styled.div`
+  ${({ theme }) => css`
+    ${media.greaterThan('medium')`
+			margin-left: ${theme.spacings.small};
+		`}
+  `}
+`
+
+export const NavMenuLink = styled.a`
+  ${({ theme }) => css`
+    position: relative;
+    color: ${theme.colors.white};
+    font-size: ${theme.font.sizes.medium};
+    margin: 0.3rem ${theme.spacings.small} 0;
+    text-decoration: none;
+    text-align: center;
+    &:hover {
+      &::after {
+        content: '';
+        position: absolute;
+        display: block;
+        height: 0.3rem;
+        background-color: ${theme.colors.primary};
+        animation: hoverAnimation 0.2s forwards;
+      }
+      @keyframes hoverAnimation {
+        from {
+          width: 0;
+          left: 50%;
         }
-    `}
+        to {
+          width: 100%;
+          left: 0;
+        }
+      }
+    }
+  `}
 `
 
 type FullMenuProps = {
-    isOpen: boolean
+  isOpen: boolean
 }
 
 export const FullMenu = styled.nav<FullMenuProps>`
-    opacity: ${({ isOpen }) => isOpen ? 1 : 0 };
+  ${({ theme, isOpen }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background: ${theme.colors.white};
+    position: fixed;
+    z-index: ${theme.layers.menu};
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100vh;
+    overflow: hidden;
+    transition: opacity 0.3s ease-in-out;
+    opacity: ${isOpen ? 1 : 0};
+    pointer-events: ${isOpen ? 'all' : 'none'};
+    > svg {
+      position: absolute;
+      top: 0;
+      right: 0;
+      margin: ${theme.spacings.xsmall};
+      cursor: pointer;
+      width: 2.4rem;
+      height: 2.4rem;
+    }
+    ${NavMenu} {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex: 1;
+      flex-direction: column;
+    }
+    ${NavMenuLink} {
+      color: ${theme.colors.black};
+      font-weight: ${theme.font.bold};
+      font-size: ${theme.font.sizes.xlarge};
+      margin-bottom: ${theme.spacings.small};
+      transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
+      transition: transform 0.3s ease-in-out;
+    }
+    ${RegisterBox} {
+      transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
+      transition: transform 0.3s ease-in-out;
+    }
+  `}
+`
+
+export const RegisterBox = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 ${theme.spacings.xlarge} ${theme.spacings.xlarge};
+    > span {
+      display: block;
+      margin: ${theme.spacings.xxsmall} 0;
+      font-size: ${theme.font.sizes.xsmall};
+    }
+  `}
+`
+
+export const CreateAccountLink = styled.a`
+  ${({ theme }) => css`
+    text-decoration: none;
+    color: ${theme.colors.primary};
+    border-bottom: 0.2rem solid ${theme.colors.primary};
+  `}
 `
